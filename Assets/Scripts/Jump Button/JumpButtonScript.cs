2fde9c89cf4d8a77635931590abc064e8472b87d
Ceren Avci <s.cerenavci@gmail.com>
2017-05-28 23:56:40 +0300

    Prepared The Jump Logic

---
+++ Assets/Scripts/Jump Button/JumpButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class JumpButtonScript : MonoBehaviour, IPointerUpHandler, IPointerDownHandler {
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (PlayerJumpScript.instance!=null)
+        {
+            PlayerJumpScript.instance.SetPower(true);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (PlayerJumpScript.instance != null)
+        {
+            PlayerJumpScript.instance.SetPower(false);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Player Scripts/PlayerJumpScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerJumpScript : MonoBehaviour
+{
+
+
+    public static PlayerJumpScript instance;
+
+    private Rigidbody2D myBody;
+
+    private Animator anim;
+
+    private float forceX, forceY;
+
+    private float thresholdx = 7f;
+
+    private float thresholdY = 14f;
+
+    private bool didJump, setPower;
+
+    void Awake()
+    {
+        MakeInstance();
+    }
+
+    void MakeInstance()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+    }
+
+    public void SetPower(bool setPower)
+    {
+        this.setPower = setPower;
+
+        if (setPower)
+            Debug.Log("We Are Settig The Power");
+        else
+            Debug.Log("We Are Not Settig The Power");
+
+
+    }
+}
