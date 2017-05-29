5a9d0825eb85d9ca3e5fe046ce8832e1a59acae3
Ceren Avci <s.cerenavci@gmail.com>
2017-05-29 23:33:37 +0300

    Programmed The Logic Of Jumping

--- Assets/Scripts/Player Scripts/PlayerJumpScript.cs
+++ Assets/Scripts/Player Scripts/PlayerJumpScript.cs
@@ -11,9 +11,11 @@ public class PlayerJumpScript : MonoBehaviour
 
     private Animator anim;
 
+    [SerializeField]
+
     private float forceX, forceY;
 
-    private float thresholdx = 7f;
+    private float thresholdX = 7f;
 
     private float thresholdY = 14f;
 
@@ -22,8 +24,20 @@ public class PlayerJumpScript : MonoBehaviour
     void Awake()
     {
         MakeInstance();
+        Initialize();
     }
 
+    void Update()
+    {
+        SetPower();
+    }
+
+    void Initialize()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
     void MakeInstance()
     {
         if (!instance)
@@ -32,15 +46,53 @@ public class PlayerJumpScript : MonoBehaviour
         }
     }
 
-    public void SetPower(bool setPower)
+    public void SetPower()
     {
         this.setPower = setPower;
 
         if (setPower)
-            Debug.Log("We Are Settig The Power");
+        {
+            forceX += thresholdX * Time.deltaTime;
+            forceY += thresholdY * Time.deltaTime;
+
+            if (forceX > 6.5f)
+                forceX = 6.5f;
+            if (forceY > 13.5f)
+                forceY = 6.5f;
+        }
         else
             Debug.Log("We Are Not Settig The Power");
 
 
+    }
+
+    public void SetPower(bool setPower)
+    {
+        this.setPower = setPower;
+
+        if (!setPower)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        myBody.velocity = new Vector2(forceX, forceY);
+        forceX = forceY = 0;
+        didJump = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        
+        if (didJump)
+        {
+            didJump = false;
+            if (target.tag == "Platform")
+            {
+                Debug.Log("Landed on Platform After Jumping");
+            }
+        }
     }
 }
